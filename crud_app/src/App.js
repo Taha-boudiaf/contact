e391a6d3c57c75ce72d3ead4496125b2340fbfa2
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-11-04 20:13:05 +0100

    install material ui

--- crud_app/src/App.js
+++ crud_app/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./component/layout/Navbar";
+import ContactList from "./component/ContactList";
+import ContactForm from "./component/ContactForm";
 
+import "./App.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <ContactList />
+      <ContactForm />
+    </div>
+  );
 }
 
 export default App;
