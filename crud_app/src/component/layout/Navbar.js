526e07aa826c4181ede291a9a399744e06d1cb77
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-11-17 00:10:54 +0100

    designed a nav

--- crud_app/src/component/layout/Navbar.js
+++ crud_app/src/component/layout/Navbar.js
@@ -1,11 +1,43 @@
-import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 
-const Navbar = () => {
-  return (
-    <div>
-      <p>navbar</p>
-    </div>
-  );
+function ElevationScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
 };
 
-export default Navbar;
+export default function ElevateAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              CRUD system test
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </React.Fragment>
+  );
+}
