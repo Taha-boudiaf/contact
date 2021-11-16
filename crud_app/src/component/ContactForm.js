eb82da76bc164afbed11e1fe8b9a2fd1b9783670
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-11-17 00:54:33 +0100

    create input and button & styled contact form

--- crud_app/src/component/ContactForm.js
+++ crud_app/src/component/ContactForm.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const ContactForm = () => {
   return (
-    <div>
-      <p>contact form</p>
-    </div>
+    <Box style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      <Typography variant="h5">Add Contact</Typography>
+      <TextField label="userName" style={{ marginTop: "10px" }} />
+      <TextField label="Email" style={{ marginTop: "10px" }} />
+      <Button variant="contained" style={{ marginTop: "20px" }}>
+        Add Contact
+      </Button>
+    </Box>
   );
 };
 
